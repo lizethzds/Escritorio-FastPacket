06fd14ab1e748d5b2942b71a319143410b365f03
UrielVell <urielvh84@gmail.com>
2024-11-30 23:50:54 -0600

    Agregar unidades

--- EscritorioFast-Packet/src/escritoriofastpacket/vista/unidades/FXMLAdminUnidadesController.java
+++ EscritorioFast-Packet/src/escritoriofastpacket/vista/unidades/FXMLAdminUnidadesController.java
@@ -5,41 +5,87 @@
  */
 package escritoriofastpacket.vista.unidades;
 
+import escritoriofastpacket.interfaz.INotificarOperacion;
+import escritoriofastpacket.modelo.dao.UnidadDAO;
+import escritoriofastpacket.modelo.pojo.Unidad;
+import escritoriofastpacket.utils.Utilidades;
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author lizet
  */
-public class FXMLAdminUnidadesController implements Initializable {
+public class FXMLAdminUnidadesController implements Initializable, INotificarOperacion {
+    
+    
+
+    private ObservableList<Unidad> unidades;
 
     @FXML
-    private TableColumn<?, ?> col_vin;
+    private TableColumn col_vin;
     @FXML
-    private TableColumn<?, ?> col_marca;
+    private TableColumn col_marca;
     @FXML
-    private TableColumn<?, ?> col_modelo;
+    private TableColumn col_modelo;
     @FXML
-    private TableColumn<?, ?> col_tipo;
+    private TableColumn col_tipo;
     @FXML
-    private TableColumn<?, ?> col_anio;
+    private TableColumn col_anio;
+    @FXML
+    private TableColumn col_identificador;
+    @FXML
+    private TableView<Unidad> tv_unidades;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        configurarTabla();
+        cargarInformacionTabla();
+    }
+
+    private void configurarTabla() {
+        col_anio.setCellValueFactory(new PropertyValueFactory("anio"));
+        col_identificador.setCellValueFactory(new PropertyValueFactory("noIdentificacion"));
+        col_marca.setCellValueFactory(new PropertyValueFactory("marca"));
+        col_modelo.setCellValueFactory(new PropertyValueFactory("modelo"));
+        col_tipo.setCellValueFactory(new PropertyValueFactory("tipoUnidad"));
+        col_vin.setCellValueFactory(new PropertyValueFactory("vin"));
+    }
+
+    private void cargarInformacionTabla() {
+        unidades = FXCollections.observableArrayList();
+        List<Unidad> listaWS = UnidadDAO.obtenerUnidades();
+        if (listaWS != null) {
+            unidades.addAll(listaWS);
+            tv_unidades.setItems(unidades);
+        } else {
+            Utilidades.mostrarAlertaSimple("Datos no disponibles", "No se pudieron cargar los datos en la tabla", Alert.AlertType.ERROR);
+        }
+    }
 
     @FXML
     private void btnAgregarUnidad(ActionEvent event) {
+        irFormularioAgregarUnidad(this, null);
     }
 
     @FXML
@@ -57,5 +103,30 @@ public class FXMLAdminUnidadesController implements Initializable {
     @FXML
     private void btnAsignarConductor(ActionEvent event) {
     }
-    
+
+    private void irFormularioAgregarUnidad(INotificarOperacion observador, Unidad unidad) {
+        try {
+            Stage escenario = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLAgregarUnidad.fxml"));
+            Parent vista = loader.load();
+            FXMLAgregarUnidadController controller = loader.getController();
+            controller.inicializarValores(observador, unidad);
+            
+            Scene escena = new Scene(vista);
+            escenario.setScene(escena);
+            escenario.setTitle("Foprmulario de Unidad");
+            escenario.initModality(Modality.APPLICATION_MODAL);
+            escenario.showAndWait();
+            escenario.setResizable(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Utilidades.mostrarAlertaSimple("Error inesperado", "Ocurrio un error al mostrar el formulario, intentelo de nuevo.", Alert.AlertType.ERROR);
+        }
+    }
+
+    @Override
+    public void notificarOperacionExitosa(String tipo, String nombre) {
+         cargarInformacionTabla();
+    }
+
 }
--- EscritorioFast-Packet/src/escritoriofastpacket/vista/unidades/FXMLAgregarUnidadController.java
+++ EscritorioFast-Packet/src/escritoriofastpacket/vista/unidades/FXMLAgregarUnidadController.java
@@ -5,13 +5,24 @@
  */
 package escritoriofastpacket.vista.unidades;
 
+import escritoriofastpacket.interfaz.INotificarOperacion;
+import escritoriofastpacket.modelo.dao.UnidadDAO;
+import escritoriofastpacket.modelo.pojo.Mensaje;
+import escritoriofastpacket.modelo.pojo.TipoUnidad;
+import escritoriofastpacket.modelo.pojo.Unidad;
+import escritoriofastpacket.utils.Utilidades;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,6 +30,12 @@ import javafx.scene.control.TextField;
  * @author uriel
  */
 public class FXMLAgregarUnidadController implements Initializable {
+    
+    private INotificarOperacion observador;
+    private Unidad unidadEdiatada;
+    private boolean modoEdicion = false;
+    private ObservableList<TipoUnidad> tipoUnidad;
+    
 
     @FXML
     private TextField tf_vin;
@@ -29,22 +46,76 @@ public class FXMLAgregarUnidadController implements Initializable {
     @FXML
     private TextField tf_anio;
     @FXML
-    private ComboBox<?> cb_tipo;
+    private ComboBox<TipoUnidad> cb_tipo;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        cargarTipoUnidad();
+    }
+
+    public void inicializarValores(INotificarOperacion observador, Unidad unidadEditada){
+        this.observador = observador;
+        this.unidadEdiatada = unidadEditada;
+        if (unidadEditada != null) {
+            modoEdicion = true;
+            cargarDatosEdicion();
+        }
+    }
+    
+    private void cargarDatosEdicion(){
+        
+    }
 
     @FXML
     private void btnGuardarUnidad(ActionEvent event) {
+        Unidad unidad = new Unidad();
+        String anio = tf_anio.getText();
+        String marca = tf_marca.getText();
+        String modelo = tf_modelo.getText();
+        String vin = tf_vin.getText();
+                
+        String noIdentificacion = vin.substring(0,4) + anio;
+        int idTipoUnidad = (cb_tipo.getSelectionModel().getSelectedItem() != null) 
+                ? cb_tipo.getSelectionModel().getSelectedItem().getIdTipoUnidad()
+                : null;
+        
+        unidad.setAnio(anio);
+        unidad.setMarca(marca);
+        unidad.setModelo(modelo);
+        unidad.setVin(vin);
+        unidad.setNoIdentificacion(noIdentificacion);
+        unidad.setIdTipoUnidad(idTipoUnidad);
+        Mensaje respuesta = UnidadDAO.registrarUnidad(unidad);
+        if (respuesta.isError()) {
+            Utilidades.mostrarAlertaSimple("Error", respuesta.getContenido(), Alert.AlertType.ERROR);
+        }else{
+            Utilidades.mostrarAlertaSimple("Registro", "Registro realizado correctamente", Alert.AlertType.INFORMATION);
+            cerrarVenatana();
+            observador.notificarOperacionExitosa("Registro", unidad.getVin());
+        }
     }
 
     @FXML
     private void btnCancelar(ActionEvent event) {
+        cerrarVenatana();
+    }
+    
+    private void cargarTipoUnidad(){
+        tipoUnidad = FXCollections.observableArrayList();
+        List<TipoUnidad> listaWS = UnidadDAO.obtenerTipoUnidad();
+        System.err.println(listaWS.get(0));
+        if (listaWS != null) {
+            tipoUnidad.addAll(listaWS);
+            cb_tipo.setItems(tipoUnidad);
+        }
+    }
+    
+    private void cerrarVenatana(){
+         Stage escenario = (Stage) cb_tipo.getScene().getWindow();
+        escenario.close();
     }
     
 }
